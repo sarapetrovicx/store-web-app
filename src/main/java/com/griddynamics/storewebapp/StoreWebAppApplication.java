f6de9d1fbfc476568c5fca9632f66566bab84510
sarapetrovicx <123303304+sarapetrovicx@users.noreply.github.com>
2026-04-20 10:14:20 +0200

    Add files via upload

---
+++ src/main/java/com/griddynamics/storewebapp/StoreWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.griddynamics.storewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/griddynamics/storewebapp/StoreWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.storewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
